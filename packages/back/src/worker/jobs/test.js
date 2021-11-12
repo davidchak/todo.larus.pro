d79abf56b17e53a8021e81bcd433a1a0c577a170
David <davidchak@yandex.ru>
2021-11-12 08:02:38 +0000

    change to async

--- packages/back/src/worker/jobs/test.js
+++ packages/back/src/worker/jobs/test.js
@@ -3,13 +3,15 @@ const moment = require("moment");
 const child_process = require('child_process');
 const path = require("path");
 
+async function startJob(){
+  await child_process.execFile('python', [path.resolve(__dirname, 'test.py'), [1, 2, 3, 4], JSON.stringify({"test": "ping"})], (err, stdout, stderr) => {
+    if (err) {
+      console.log(`Task ${uuidv4()} complete at ${moment().format('HH:mm:ss')} with error: ${err}`);
+      return;
+    }
 
+    console.log(`Task ${uuidv4()} complete at ${moment().format('HH:mm:ss')} with result ===> ${stdout}`);
+  });
+}
 
-const child = child_process.execFile('python', [path.resolve(__dirname, 'test.py'), [1, 2, 3, 4], JSON.stringify({"test": "ping"})], (err, stdout, stderr) => {
-  if (err) {
-    console.log(`Task ${uuidv4()} complete at ${moment().format('HH:mm:ss')} with error: ${err}`);
-    return;
-  }
-
-  console.log(`Task ${uuidv4()} complete at ${moment().format('HH:mm:ss')} with result ===> ${stdout}`);
-});
+startJob()
--- packages/back/src/worker/jobs/test.py
+++ packages/back/src/worker/jobs/test.py
@@ -1,5 +1,7 @@
 import sys
 import json
+import time
 from datetime import datetime as dt
 
-print(f"Hello from Python at {dt.now()}, args: {sys.argv[1]}, {json.loads(sys.argv[2])['test']}")
+time.sleep(3)
+print(f"Hello from Python at {dt.now()} utc, args: {sys.argv[1]}, {json.loads(sys.argv[2])['test']}")
