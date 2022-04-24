8f08c479fda5a2235c6dc03eec720abe3f8cae10
davidchak <davidchak@yandex.ru>
2022-04-24 22:03:22 +0300

    add react router

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,16 +1,18 @@
 import { ContentLayout } from './layouts/contentlayout/ContentLayout';
 import { MenuLayout } from './layouts/menuLayout/MenuLayout';
 import { FlexContainer } from './components/flexContainer/FlexContainer';
-import { DirectionEnum } from './components/flexContainer/FlexContainer.types';
+import { BrowserRouter } from 'react-router-dom';
 import styles from './App.module.scss';
 
 export const App = () => {
 	return (
 		<div className={styles.app}>
-			<FlexContainer >
-				<MenuLayout />
-				<ContentLayout />
-			</FlexContainer>
+			<BrowserRouter>
+				<FlexContainer >
+					<MenuLayout />
+					<ContentLayout />
+				</FlexContainer>
+			</BrowserRouter>
 		</div>
 	);
 }
