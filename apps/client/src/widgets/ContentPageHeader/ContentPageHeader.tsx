9ec8564d59b716f174df606b748fea04de80cb6b
davidchak <davidchak@yandex.ru>
2023-03-05 22:08:22 +0300

    move logics to features

--- apps/client/src/widgets/ContentPageHeader/ContentPageHeader.tsx
+++ apps/client/src/widgets/ContentPageHeader/ContentPageHeader.tsx
@@ -1,8 +1,6 @@
 import { Button } from "@consta/uikit/Button"
 import { Layout } from "@consta/uikit/Layout"
-import { TaskEventBus } from "features/bus";
-import { IUserModel, useUserStore } from "entities/User/model";
-import { useTaskStore } from "entities/Task";
+import { addNewTaskAsync } from "features/Task";
 import { v4 } from "uuid";
 
 export interface IContentPageHeaderProps { 
@@ -10,25 +8,15 @@ export interface IContentPageHeaderProps {
 }
 
 export const ContentPageHeader = ({ page }: IContentPageHeaderProps) => {
-	const createTaskAsync = useTaskStore((state) => state.createTaskAsync);
-	const createUserAsync = useUserStore((state) => state.createUserAsync)
-
-	// FIXME: Вынести в features создание новой таски
-	// FIXME: Вынести в features отображение списка задач
+	// TODO: переписать на открытие модалки для создания задачи
 	const handleClick = async () => {
 		const ch = v4();
-		const newTask = await createTaskAsync({
+		await addNewTaskAsync({
 			title: `Task title ${ch}`,
 			description: `Task description ${ch}`,
 			done: false,
-			owner: await createUserAsync({
-				email: "admin@mail.ru",
-				authToken: "notoken",
-				active: true,
-				lastLogin: new Date()
-			}) as IUserModel
+			owner: null
 		})
-		await TaskEventBus.emit(newTask);
 	}
 
 	return (<Layout style={{ height: "64px", gap: "64px", paddingLeft: "10px", alignItems: "center" }}>
