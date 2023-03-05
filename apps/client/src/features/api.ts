447ad9fe01360ad11611182e4610d1954dc08230
davidchak <davidchak@yandex.ru>
2023-03-05 22:13:06 +0300

    move logic

--- apps/client/src/features/api.ts
+++
@@ -1,8 +0,0 @@
-// README: Интерфейс для управления всем приложением
-
-import * as Task from "./Task";
-
-// TODO: зарегистрировать в window.api (window.api.task.addNewTask)
-const features = {
-	Task,
-}
