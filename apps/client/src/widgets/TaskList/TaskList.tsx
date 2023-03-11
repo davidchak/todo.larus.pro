8e874e531a854ac6cb4f6ccbc4c3cfc26ccb7ebb
davidchak <davidchak@yandex.ru>
2023-03-11 17:49:14 +0300

    update styles

--- apps/client/src/widgets/TaskList/TaskList.tsx
+++ apps/client/src/widgets/TaskList/TaskList.tsx
@@ -6,7 +6,8 @@ import { useTaskStore, TaskRowUi } from "entities/Task";
 export const TaskList = () => {
 	const taskList = useTaskStore((state) => state.taskList);
 
-	return (<Layout direction="column" style={{ overflowY: "scroll" }}>
+	return (<Layout direction="column" style={{
+		overflowY: "scroll", background: "rgba(247, 248, 249, .66)", backdropFilter: "blur(20px)", height: "inherit" }}>
 		{ taskList.map(task => <TaskRowUi key={ task.id } task={ task } />) }
 	</Layout>)
 }
